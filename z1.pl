b2f2eab01cc962f00bc4c8132f18e0cabbd23bb3
ddshel <d-d-sh@mail.ru>
2018-04-13 18:00:32 +0300

    z5

--- z1.pl
+++ z1.pl
@@ -1,7 +1,15 @@
 #!/usr/bin/perl -w
 use strict;
-my $v = <STDIN>;
-chomp $v;
-my $t = <STDIN>;
-chomp $t;
-print (($v*$t)%109);
+my $n = <STDIN>;
+chomp $n;
+my $min;
+my $hour;
+if($n%2==0){
+	$min = (45*$n + ($n/2-1)*15 + ($n/2)*5);
+}
+else{
+	$min = (45*$n + (($n-1)/2)*15 + (($n-1)/2)*5);
+}
+$hour = (9 + ($min - ($min%60))/60);
+	$min = $min%60;
+	print $hour.' '.$min;
