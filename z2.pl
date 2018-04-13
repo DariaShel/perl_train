aa4455bb8e040081b1e3236c97d167b15ff61fcd
ddshel <d-d-sh@mail.ru>
2018-04-13 19:11:29 +0300

    z6

--- z2.pl
+++ z2.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl -w
 use strict;
-my $x = <STDIN>;
-chomp $x;
-my $y = <STDIN>;
-chomp $y;
-if($x>$y){print $x;}
-else{print $y;}
+my $a = <STDIN>;
+chomp $a;
+my $b = <STDIN>;
+chomp $b;
+my $c = <STDIN>;
+chomp $c;
+if(($a+$b>$c)and($a+$c>$b)and($c+$b>$a)){print 'YES';}
+else{print 'NO';}
