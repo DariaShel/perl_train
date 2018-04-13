2991bdbae7d1f5da84ea4ca2c404012d19006ea8
ddshel <d-d-sh@mail.ru>
2018-04-13 19:24:08 +0300

    z9

--- z2.pl
+++ z2.pl
@@ -2,25 +2,12 @@
 use strict;
 my $n = <STDIN>;
 chomp $n;
-my $m = <STDIN>;
-chomp $m;
-my $x = <STDIN>;
-chomp $x;
-my $y = <STDIN>;
-chomp $y;
-my $a;
-my $b;
-if($n<$m){
-	if($x<($n-$x)){$a=$x;}
-	else{$a=$n-$x;}
-	if($y<($m-$y)){$b=$y;}
-	else{$b=$m-$y;}
+my $i;
+my $sum = 0;
+for($i=0;$i<$n;$i++){
+	my $x = <STDIN>;
+	chomp $x;
+	$sum += $x;
 }
-else{
-	if($x<($m-$x)){$a=$x;}
-	else{$a=$m-$x;}
-	if($y<($n-$y)){$b=$y;}
-	else{$b=$n-$y;}
-}
-if($a>$b){print $b;}
-else{print $a;}
+print $sum;
+
