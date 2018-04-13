42e5d133e1dfe5046450897fcb6a9919fbf804a3
ddshel <d-d-sh@mail.ru>
2018-04-13 19:36:32 +0300

    z11

--- z2.pl
+++ z2.pl
@@ -1,14 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
-my $a = <STDIN>;
-chomp $a;
-my $b = <STDIN>;
-chomp $b;
-while($a%2!=0){
-	$a+=1;
-}
-while($a<=$b){
-	print $a.' ';
-	$a+=2;
+my $i;
+for($i=10;$i<100;$i++){
+	if($i==2*($i%10)*(($i-$i%10)/10)){print $i.' ';}
 }
 
