1a77681f712dd5f50990045dcb6ec772a2c678d6
ddshel <d-d-sh@mail.ru>
2018-04-20 17:53:29 +0300

    z15

--- z2.pl
+++ z2.pl
@@ -3,9 +3,9 @@ use strict;
 my $n = <STDIN>;
 chomp $n;
 my $i;
-my $fact = 1;
-for($i=1;$i<=$n;$i++){
-	$fact = $fact*$i;
+my $count = 0;
+foreach $i (split(' ',$n)){
+	$count += 1;
 }
-print $fact;
+print $count;
 
