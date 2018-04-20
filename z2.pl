87d5d3d8848d68a197cd4436dd270445a2b3b13e
ddshel <d-d-sh@mail.ru>
2018-04-20 17:22:12 +0300

    z14

--- z2.pl
+++ z2.pl
@@ -3,7 +3,9 @@ use strict;
 my $n = <STDIN>;
 chomp $n;
 my $i;
-for($i=100;$i<1000;$i++){
-	if($n==(($i%10)+(($i-$i%100) / 100)+(($i%100-$i%10) / 10))){print $i.' ';}
+my $fact = 1;
+for($i=1;$i<=$n;$i++){
+	$fact = $fact*$i;
 }
+print $fact;
 
