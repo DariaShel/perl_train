f328afbb0b2c82ecf8160b1405d1f995e382f7e5
ddshel <d-d-sh@mail.ru>
2018-04-22 23:27:42 +0300

    z18

--- z2.pl
+++ z2.pl
@@ -3,17 +3,10 @@ use strict;
 my $n = <STDIN>;
 chomp $n;
 my $i;
-my $index;
-my $tmp=0;
-my $count = 0;
 foreach $i (split('',$n)){
-	if($i eq 'f'){
-		$count+=1;
-		if($count == 2){$index = $tmp;}
+	if($i eq '1'){
+		$i = "one";
 	}
-	$tmp += 1;
+	print $i;
 }
-if($count == 0){print -2;}
-elsif($count == 1){print -1;}
-elsif($count >= 2){print $index;}
 
