acd76558d9eb6f65f8b529d35c8e51a51c11b229
ddshel <d-d-sh@mail.ru>
2018-04-22 23:03:23 +0300

    z16

--- z2.pl
+++ z2.pl
@@ -3,9 +3,20 @@ use strict;
 my $n = <STDIN>;
 chomp $n;
 my $i;
+my $index1;
+my $index2;
+my $tmp=0;
 my $count = 0;
-foreach $i (split(' ',$n)){
-	$count += 1;
+foreach $i (split('',$n)){
+	if($i eq 'f'){
+		$count+=1;
+		if($count == 1){$index1 = $tmp;}
+		elsif($count > 1){
+			$index2 = $tmp;
+		}
+	}
+	$tmp += 1;
 }
-print $count;
+if($count == 1){print $index1;}
+elsif($count > 1){print $index1." ".$index2;}
 
