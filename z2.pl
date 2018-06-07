2de386758d0a3f901a27a5af9401696dac830939
ddshel <d-d-sh@mail.ru>
2018-06-07 16:47:11 +0300

    zadacha B

--- z2.pl
+++ z2.pl
@@ -1,16 +1,10 @@
 #!/usr/bin/perl -w
 use strict;
-my $n = <STDIN>;
-chomp $n;
-my $res;
-if((ord($n)>=97)and(ord($n)<=122)){
-	$res = chr(ord($n)-32);
-}
-elsif((ord($n)>=65)and(ord($n)<=90)){
-	$res = chr(ord($n)+32);
-}
-else{
-	$res = $n;
-}
-print $res;
+my $str1 = <STDIN>;
+chomp $str1;
+my $str2 = <STDIN>;
+chomp $str2;
+if($str1 eq $str2){print "yes";}
+else{print "no"}
+
 
