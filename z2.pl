c342ae2d7de0d1cb356cde9a9254c4c3c20ac7c1
ddshel <d-d-sh@mail.ru>
2018-06-07 16:57:09 +0300

    zadacha D

--- z2.pl
+++ z2.pl
@@ -3,6 +3,15 @@ use strict;
 my $str = <STDIN>;
 chomp $str;
 my @arr = split(" ",$str);
-print scalar(@arr);
+my $i;
+my $max = 0;
+my $tmp;
+foreach $i (@arr){
+	if($max < length($i)){
+		$max = length($i);
+		$tmp = $i;
+	}
+}
+print $tmp,"\n",$max;
 
 
