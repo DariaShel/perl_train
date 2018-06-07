1add478c171fdd84d176f385a7dd06a0c97aa84e
ddshel <d-d-sh@mail.ru>
2018-06-07 17:52:54 +0300

    zadacha G (chast resh)

--- z2.pl
+++ z2.pl
@@ -2,14 +2,14 @@
 use strict;
 my $str = <STDIN>;
 chomp $str;
-my @arr = split("",$str);
+my @arr = split(" ",$str);
+my $x = 0;
+my $y = 0;
 my $i;
-my $j;
-for($i=0;$i<length($str);$i++){
-	for($j=$i+1;$j<length($str);$j++){
-		if($arr[$i] eq $arr[$j]){
-			print $arr[$i];
-			last;
-		}
-	}
+for($i=0;$i<scalar(@arr);$i+=2){
+	if($arr[$i] eq "North"){$y = $arr[$i+1] + $y;}
+	elsif($arr[$i] eq "South"){$y = $arr[$i+1]*(-1) + $y;}
+	elsif($arr[$i] eq "East"){$x = $arr[$i+1] + $x;}
+	elsif($arr[$i] eq "West"){$x = $arr[$i+1]*(-1) + $x;}
 }
+print $x," ",$y;
