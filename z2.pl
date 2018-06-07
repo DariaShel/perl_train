85e2da61e3f49bee5dd6eeda235ec835461746fa
ddshel <d-d-sh@mail.ru>
2018-06-07 16:53:38 +0300

    zadacha C

--- z2.pl
+++ z2.pl
@@ -1,10 +1,8 @@
 #!/usr/bin/perl -w
 use strict;
-my $str1 = <STDIN>;
-chomp $str1;
-my $str2 = <STDIN>;
-chomp $str2;
-if($str1 eq $str2){print "yes";}
-else{print "no"}
+my $str = <STDIN>;
+chomp $str;
+my @arr = split(" ",$str);
+print scalar(@arr);
 
 
